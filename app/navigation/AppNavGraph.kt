fa5cc2a0e0a6e9531a5fbba0026775ac5cb9aec7
Sakhilekhuzwayo <Sakhilekhuzwayo454@gmail.com>
2025-05-11 18:10:17 +0200

    Add navigation files and update manifest/styles

---
+++ app/navigation/AppNavGraph.kt
@@ -0,0 +1,19 @@
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.masterbudgetapp.R
+
+class AppNavGraph : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_app_nav_graph)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
