12f404d01309bc83433294c59b8905c476c8ab53
Sakhilekhuzwayo <Sakhilekhuzwayo454@gmail.com>
2025-05-12 22:58:17 +0200

    added RegisterViewModel.kt and gradlefile, libs.versions.toml

--- app/src/main/java/com/example/masterbudgetapp/ui/screens/registerScreen/RegisterViewModel.kt
+++ app/src/main/java/com/example/masterbudgetapp/ui/screens/registerScreen/RegisterViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.masterbudgetapp.ui.screens.registerScreen
 
-class RegisterViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.masterbudgetapp.data.repository.UserRepository
+import kotlinx.coroutines.launch
+
+class RegisterViewModel(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    val registerState = MutableLiveData<Result<Boolean>>() // or use StateFlow
+
+    fun registerUser(name: String, email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                registerState.value = Result.Loading
+                val result = userRepository.registerUser(name, email, password)
+                registerState.value = Result.Success(result)
+            } catch (e: Exception) {
+                registerState.value = Result.Error(e)
+            }
+        }
+    }
 }
